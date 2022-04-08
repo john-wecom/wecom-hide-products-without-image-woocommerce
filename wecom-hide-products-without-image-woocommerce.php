39777746932aeed0c4d95aa304c6223b240c784e
Ilias P <ilias.p@wecommerce.gr>
2022-04-08 11:12:06 +0300

    Initial commit

---
+++ wecom-hide-products-without-image-woocommerce.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Hide products without image from the shop loop
+ */
+add_action( 'woocommerce_product_query', 'wecom_product_query_remove_products_with_missing_thumbnails', 20 );
+if ( ! function_exists( 'wecom_product_query_remove_products_with_missing_thumbnails' ) ) {
+	function wecom_product_query_remove_products_with_missing_thumbnails( $q ) {
+
+		$meta_query = $q->get( 'meta_query' );
+
+		$meta_query[] = array(
+			array(
+				'key'     => '_thumbnail_id',
+				'compare' => 'EXISTS',
+			),
+		);
+
+		$q->set( 'meta_query', $meta_query );
+	}
+}
+
+/*
+ * Hide products without image from [products] shortcode
+ */
+add_filter( 'woocommerce_shortcode_products_query', 'wecom_products_shortcode_remove_products_with_missing_thumbnails', 10, 1 );
+if ( ! function_exists( 'wecom_products_shortcode_remove_products_with_missing_thumbnails' ) ) {
+	function wecom_products_shortcode_remove_products_with_missing_thumbnails( $query_args ) {
+		$meta_query = $query_args['meta_query'];
+		if ( ! empty( $meta_query ) ) {
+			$query_args['meta_query'] = array(
+				$meta_query,
+				array(
+					'key'     => '_thumbnail_id',
+					'compare' => 'EXISTS',
+				),
+			);
+		} else {
+			$query_args['meta_query'] = array(
+				array(
+					'key'     => '_thumbnail_id',
+					'compare' => 'EXISTS',
+				),
+			);
+		}
+		return $query_args;
+	}
+}
